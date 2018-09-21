14b57ab9c8e935413f5fd612c360a68bba232108
Sivodedov Dmitry <intetm@gmail.com>
2018-09-21 11:35:43 +0300

    fix file

--- plugin/src/main/java/com/sbrf/meta/plugin/launch/ParserMojo.java
+++ plugin/src/main/java/com/sbrf/meta/plugin/launch/ParserMojo.java
@@ -33,7 +33,9 @@ public class ParserMojo extends AbstractMojo {
     public void execute() {
         getLog().info("Start parse");
         Set<File> jars = getDependency();
-        jars.add(getJar());
+        File jar = getJar();
+        if (jar != null)
+            jars.add(jar);
         Collection<File> source = new ArrayList<>();
         collectFileFromDir(new File(sourceDir), source);
         ApiStorage storage = Parser.parse(jars, source);
@@ -44,8 +46,11 @@ public class ParserMojo extends AbstractMojo {
     public Set<File> getDependency() {
         Set<File> result = new HashSet<>();
         for (Artifact artifact : project.getArtifacts()) {
-            result.add(artifact.getFile());
-            getLog().info("jar " + artifact.getFile().getAbsolutePath());
+            File file = artifact.getFile();
+            if (file == null)
+                continue;
+            result.add(file);
+            getLog().info("jar " + file.getAbsolutePath());
         }
         return result;
     }
@@ -54,7 +59,11 @@ public class ParserMojo extends AbstractMojo {
     }
 
     public void collectFileFromDir(File dir, Collection<File> result) {
+        if (dir == null)
+            return;
         File[] files = dir.listFiles();
+        if (files == null)
+            return;
         for (File file : files) {
             if (file.isDirectory())
                 collectFileFromDir(file, result);
