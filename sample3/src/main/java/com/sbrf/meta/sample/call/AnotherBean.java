04d324986f6c072f98cec3d9b3466615e60c1fbb
Сиводедов Дмитрий Александрович <Sivodedov.D.A@sberbank.ru>
2018-10-01 10:30:19 +0300

    Sample 2-3

---
+++ sample3/src/main/java/com/sbrf/meta/sample/call/AnotherBean.java
@@ -0,0 +1,17 @@
+package com.sbrf.meta.sample.call;
+
+import com.sbrf.meta.sample.request.SomeServiceRequest;
+
+public class AnotherBean {
+    private SomeServiceRequest serviceRequest;
+
+
+    public void testCall() {
+        System.out.println("test!!!");
+        serviceRequest.getUser(3);
+    }
+
+    public void testCallFunction() {
+        testCall();
+    }
+}
